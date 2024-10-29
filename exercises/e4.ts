5a11b3b88f118c5b10b3012dd31dd826c2b097b1
luishsilva <luizhrick@gmail.com>
2024-10-29 07:53:47 -0600

    Return all planet names with gravity less than 10

--- exercises/e4.ts
+++ exercises/e4.ts
@@ -1,8 +1,21 @@
 // SPACE DATA EXERCISE 4
 // Return an array of Planets' names with gravity less than 10
 // Return example: ['name1', 'name2', ... , 'nameN']
+import { data } from '../data/data';
 
-export function getPlanetNamesWithLowGravity(planets) {}
+interface PlanetsType {
+    name: string;
+    gravity: number;
+}
+
+export function getPlanetNamesWithLowGravity(planets: PlanetsType[]) {
+    return planets
+    .filter((planet) => planet.gravity < 10)
+    .map(planet => planet.name);
+}
+
+const { planets } = data;
+getPlanetNamesWithLowGravity(planets);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-4"
