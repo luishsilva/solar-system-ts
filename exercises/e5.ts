84165e04376d233127bc9a180cc97f8a97726158
luishsilva <luizhrick@gmail.com>
2024-10-30 06:56:38 -0600

    Return planets names with mass greater than or equal a given number

--- exercises/e5.ts
+++ exercises/e5.ts
@@ -2,10 +2,43 @@
 // Return an array with all Planets' names with a massValue greater or equal to a given number (1-7)
 // Return example: ['name1', 'name2', ... , 'nameN']
 // Note: This input should be destructured from the function parameter
+import { data } from '../data/data';
+
+interface PlanetType {
+  id: string,
+  name: string,
+  isPlanet: boolean,
+  mass: { massValue: number, massExponent: number },
+  vol: { volValue: number, volExponent: number },
+  gravity: number,
+  avgTemp: number,
+  moonsCount?: number,
+  moons?: string[],
+}
+
+interface MethodType {
+  planets: PlanetType[];
+  greaterThanOrEqualTo: number;
+}
+
 export function getPlanetNamesWithMassValue({
   planets,
   greaterThanOrEqualTo,
-}) {}
+}: MethodType) {
+  const result = planets
+    .filter(planet => planet.mass.massValue >= greaterThanOrEqualTo)
+    .map(planet => planet.name);
+  return result;
+}
+
+const { planets } = data;
+
+const param = {
+  planets: planets,
+  greaterThanOrEqualTo: 5.97237
+}
+
+getPlanetNamesWithMassValue(param)
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
