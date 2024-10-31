e8292d8656d506cc47f26d44f061aac0d6ec1825
luishsilva <luizhrick@gmail.com>
2024-10-31 15:10:19 -0600

    Return asteroid object data based in a asteroid name

--- exercises/e10.ts
+++ exercises/e10.ts
@@ -1,7 +1,34 @@
 // SPACE DATA EXERCISE 10
 // Return a given asteroid object of data
 // make sure that you destructure your parameters
-export function getAsteroidDataByName({ allData, asteroidName }) {}
+import { data } from '../data/data';
+
+interface Asteroid {
+    name: string, 
+    discoveryYear: number, 
+    orbitalPeriod: number
+}
+
+interface Data {
+    asteroids: Asteroid[];
+}
+
+interface AsteroidData {
+    allData: Data,
+    asteroidName: string
+}
+
+export function getAsteroidDataByName({ allData, asteroidName }: AsteroidData) {
+    const result = allData.asteroids.find(asteroid => asteroid.name === asteroidName)
+    return result;
+}
+
+const param = {
+    allData: data,
+    asteroidName: '6 Hebe'
+  }
+
+getAsteroidDataByName(param)
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-10"
