8291d31001e2838e4f212f0ca8518ad9909c222c
luishsilva <luizhrick@gmail.com>
2024-11-01 18:10:38 -0600

    Return an array of Planets with less than 10 moons include planets with 0 moons

--- exercises/e11.ts
+++ exercises/e11.ts
@@ -1,7 +1,29 @@
 // SPACE DATA EXERCISE 11
 // Return an array of Planets with less than 10 moons
 // include planets with 0 moons
-export function lowMoonsPlanets(planets) {}
+import { data } from "../data/data";
+
+interface Planet {
+    id: string,
+    name: string,
+    isPlanet: boolean,
+    mass: { massValue: number, massExponent: number },
+    vol: { volValue: number, volExponent: number },
+    gravity: number,
+    avgTemp: number,
+    moonsCount?: number,
+    moons?: string[],
+}
+
+export function lowMoonsPlanets(planets: Planet[]) {
+    return planets.filter(planet => 
+        planet.moonsCount === undefined || planet.moonsCount < 10
+    );
+}
+
+const { planets } = data;
+
+lowMoonsPlanets(planets);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-11"
