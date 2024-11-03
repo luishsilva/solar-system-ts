8e332b87205cf3914f45ae488c809eddeb7bc9e0
luishsilva <luizhrick@gmail.com>
2024-11-03 00:24:19 -0600

    Return an array of Planets without moons

--- exercises/e15.ts
+++ exercises/e15.ts
@@ -1,7 +1,27 @@
 // SPACE DATA EXERCISE 15
 // Return an array of Planets' without moons
 
-export function getPlanetsWithNoMoons(planets) {}
+import { data } from '../data/data';
+
+interface Planets {
+    id: string,
+    name: string,
+    isPlanet: boolean,
+    mass: { massValue: number, massExponent: number },
+    vol: { volValue: number, volExponent: number },
+    gravity: number,
+    avgTemp: number,
+    moonsCount?: number,
+    moons?: string[],
+}
+
+export function getPlanetsWithNoMoons(planets: Planets[]) {
+    return planets.filter((planet) => planet.moons === undefined)
+}
+
+const { planets } = data;
+
+getPlanetsWithNoMoons(planets)
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-15"
