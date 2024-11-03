6046f9eb715391d6bec2ae03f7d4ce316fc62dae
luishsilva <luizhrick@gmail.com>
2024-11-03 00:58:08 -0600

    Return the array if callback is true

--- exercises/e16.ts
+++ exercises/e16.ts
@@ -1,4 +1,19 @@
 // see e16.md
 
 // DO NOT USE `any` for this, you will need to use generics in order to pass both the vitest tests and the tsc compiler
-export function find(array, callback) {}
+
+export function find(array: number[], callback: (value: number) => boolean) {
+    let returnArray: number[] = [];
+    array.forEach(element => {
+        if (callback(element)) {
+            returnArray.push(element)
+        }
+    });
+    console.log(returnArray)
+    return returnArray;
+}
+
+const array = [1,2,3];
+const callbackResult = (value: number) => value === 2
+
+find(array, callbackResult)
