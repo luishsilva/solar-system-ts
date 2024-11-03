fec55733cb680f22cc78f532a436944e67f64e37
luishsilva <luizhrick@gmail.com>
2024-11-02 23:12:42 -0600

    Sum all moons for all planets

--- exercises/e12.ts
+++ exercises/e12.ts
@@ -1,7 +1,32 @@
 // SPACE DATA EXERCISE 12
 // Return the sum of all moons for all planets
 // Return example: 42
-export const allPlanetsMoonsCount = (data) => {};
+import { data } from '../data/data';
+
+interface Planet {
+    id: string,
+    name: string,
+    isPlanet: boolean,
+    mass: { massValue: number, massExponent: number },
+    vol: { volValue: number, volExponent: number },
+    gravity: number,
+    avgTemp: number,
+    moonsCount?: number,
+    moons?: string[],
+}
+
+interface PlanetData {
+    planets: Planet[];
+}
+
+export const allPlanetsMoonsCount = (data: PlanetData) => {
+    return data.planets.reduce((accumulator, currentValue) => 
+        accumulator + (currentValue.moonsCount || 0), 0)
+};
+
+// const { planets } = data;
+
+allPlanetsMoonsCount(data);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-12"
